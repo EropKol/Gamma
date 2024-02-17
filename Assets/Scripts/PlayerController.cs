249feda4fa69806033739001443f6f8fa4c94c4e
Egor Kolesnikov <159160978+EropKol@users.noreply.github.com>
2024-02-17 16:51:25 +0400

    Week 1.5
    Половина задания второй недели

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float Gravity = 9.8f;
     public float JumpForce;
     public float Speed;
+    public Animator PlayerAnim;
 
     private Vector3 _moveVector;
     private float _fallVelocity = 0;
@@ -20,27 +21,34 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         _moveVector = Vector3.zero;
+        PlayerAnim.SetFloat("Speed", 0);
+        PlayerAnim.SetFloat("HorizontalSpeed", 0);
 
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             _moveVector += transform.forward;
+            PlayerAnim.SetFloat("Speed", 1);
         }
         if (Input.GetKey(KeyCode.S))
         {
             _moveVector -= transform.forward;
+            PlayerAnim.SetFloat("Speed", -1);
         }
         if (Input.GetKey(KeyCode.A))
         {
             _moveVector -= transform.right;
+            PlayerAnim.SetFloat("HorizontalSpeed", -1);
         }
         if (Input.GetKey(KeyCode.D))
         {
             _moveVector += transform.right;
+            PlayerAnim.SetFloat("HorizontalSpeed", 1);
         }
 
         if (_characterController.isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             _fallVelocity = -JumpForce;
+            PlayerAnim.SetBool("IsJumping", true);
         }
     }
 
@@ -54,6 +62,7 @@ public class PlayerController : MonoBehaviour
         if(_characterController.isGrounded)
         {
             _fallVelocity = 0;
+            PlayerAnim.SetBool("IsJumping", false);
         }
     }
 
